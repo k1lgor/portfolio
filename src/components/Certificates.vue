f1f233d62d1553bf0154dbb4fd5511f8185eabf0
Plamen Ivanov <paco.iwanow@gmail.com>
2022-03-25 11:03:52 +0200

    one cert added

--- src/components/Certificates.vue
+++ src/components/Certificates.vue
@@ -7,25 +7,57 @@
       <li data-bs-target="#certs" data-bs-slide-to="3"></li>
       <li data-bs-target="#certs" data-bs-slide-to="4"></li>
       <li data-bs-target="#certs" data-bs-slide-to="5"></li>
+      <li data-bs-target="#certs" data-bs-slide-to="6"></li>
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active" data-interval="1000">
-        <img src="../images/cert1.png" class="d-block w-70" alt="Slide 1" />
+        <img
+          src="../images/cert1.png"
+          class="d-block w-70"
+          alt="Python Basics"
+        />
       </div>
       <div class="carousel-item" data-interval="1000">
-        <img src="../images/cert2.png" class="d-block w-70" alt="Slide 2" />
+        <img
+          src="../images/cert2.png"
+          class="d-block w-70"
+          alt="Python Fundamentals"
+        />
       </div>
       <div class="carousel-item" data-interval="1000">
-        <img src="../images/cert3.png" class="d-block w-70" alt="Slide 3" />
+        <img
+          src="../images/cert3.png"
+          class="d-block w-70"
+          alt="Python Advanced"
+        />
       </div>
       <div class="carousel-item" data-interval="1000">
-        <img src="../images/cert4.png" class="d-block w-70" alt="Slide 4" />
+        <img
+          src="../images/cert4.png"
+          class="d-block w-70"
+          alt="Windows SysAdmin"
+        />
       </div>
       <div class="carousel-item" data-interval="1000">
-        <img src="../images/cert5.png" class="d-block w-70" alt="Slide 5" />
+        <img
+          src="../images/cert5.png"
+          class="d-block w-70"
+          alt="Linux SysAdmin"
+        />
       </div>
       <div class="carousel-item" data-interval="1000">
-        <img src="../images/cert6.png" class="d-block w-70" alt="Slide 6" />
+        <img
+          src="../images/cert6.png"
+          class="d-block w-70"
+          alt="OCI Foundation"
+        />
+      </div>
+      <div class="carousel-item" data-interval="1000">
+        <img
+          src="../images/cert7.png"
+          class="d-block w-70"
+          alt="DataCamp Intro to Python"
+        />
       </div>
     </div>
     <a class="carousel-control-prev" href="#certs" data-bs-slide="prev">
